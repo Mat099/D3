758b48bb0ac22fb36f095da4a29d8b78538b6688
mati <155397898+Mat099@users.noreply.github.com>
2026-05-26 13:59:31 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#inclide "User.h"
+
+int main(){
+
+    cout << "╔══════════════════════════════════════════════════╗" << endl;
+    cout << "║                 HOSPITAL APP                     ║" << endl;
+    cout << "╠══════════════════════════════════════════════════╣" << endl;
+    cout << "║                                                  ║" << endl;
+    cout << "║  1. Login                                        ║" << endl;
+    cout << "║  3. Check ER Crowding Status                     ║" << endl;
+    cout << "║  4. View FAQ & Assistance                        ║" << endl;
+    cout << "║  5. Exit                                         ║" << endl;
+    cout << "║                                                  ║" << endl;
+    cout << "╚══════════════════════════════════════════════════╝" << endl;
+    cout << "\nEnter your choice (1-5): ";
+    cin >> choice;
+
+  switch (choice){
+    case 1: {
+      login();
+      patientMenu();
+    }
+    case 2 : {
+      login
+
+    
+}
