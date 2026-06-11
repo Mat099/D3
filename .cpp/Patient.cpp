75c0794f3f3fca64af70b8377afb5298c41e86b4
mati <155397898+Mat099@users.noreply.github.com>
2026-06-11 17:33:04 +0200

    Refactor Patient login method for clarity

--- .cpp/Patient.cpp
+++ .cpp/Patient.cpp
@@ -5,13 +5,18 @@
 using namespace std;
 
 
-Patient::Patient(const string& id, const string& name, const string& surname, 
-                 const string& email, const string& phoneNumber, const string& insurance, const string& password)
+Patient::Patient(const string& id, 
+                const string& name, 
+                const string& surname, 
+                const string& email, 
+                const string& phoneNumber, 
+                const string& insurance, 
+                const string& password)
     : User(id, name, surname, email, phoneNumber, password), insurance(insurance) {
     //appointments(nullptr), payments(nullptr), medicalRecord(nullptr)
 }
 
-bool Patient::login(string& email, string& password, string& name) {
+void Patient::login(string& email, string &password, string& name) {
     cout << "╔══════════════════════════════════════════════════╗" << endl;
     cout << "║  1. Login with CIE                               ║" << endl;
     cout << "║  2. Login with SPID                              ║" << endl;
@@ -21,58 +26,37 @@ bool Patient::login(string& email, string& password, string& name) {
     int choice = 0;
     cin >> choice;   
 
-        switch (choice) {
-            case 1:
-                cout << "╔══════════════════════════════════════════════════╗" << endl;
-                cout << "║  You chose to login with CIE                     ║" << endl;
-                cout << "║  Please enter your CIE credentials:              ║" << endl;
-                cout << "║  E-mail:                                         ║" << endl;
-                cout << "║  Password:                                       ║" << endl;
-                cout << "╚══════════════════════════════════════════════════╝" << endl;
-                cin >> email;
-                cin >> password;
+    if (choice == 1) {
+        cout << "╔══════════════════════════════════════════════════╗" << endl;
+        cout << "║  You chose to login with CIE                     ║" << endl;
+        cout << "║  Please enter your CIE credentials:              ║" << endl;
+        cout << "║  E-mail:                                         ║" << endl;
+        cout << "║  Password:                                       ║" << endl;
+        cout << "╚══════════════════════════════════════════════════╝" << endl;
+    }else if (choice == 2) {
+        cout << "╔═════════════════════════════════════════════════╗" << endl;
+        cout << "║  You chose to login with SPID                   ║" << endl;
+        cout << "║  Please enter your SPID credentials:            ║" << endl; 
+        cout << "║  E-mail:                                        ║" << endl;
+        cout << "║  Password:                                      ║" << endl;
+        cout << "╚═════════════════════════════════════════════════╝" << endl;
+    }else {
+        cout << "╔═════════════════════════════════════════════════╗" << endl;
+        cout << "║  Your choice is invalid. Please enter 1 or 2.   ║" << endl;
+        cout << "╚═════════════════════════════════════════════════╝" << endl;
+    }
 
-                if (email == "wrong" || password == "wrong") {
-                    cout << "╔═════════════════════════════════════════════════╗" << endl;
-                    cout << "║  Invalid credentials. Please try again.         ║" << endl;
-                    cout << "╚═════════════════════════════════════════════════╝" << endl;
-                    return false;
-                }
-              else{
-                    cout << "╔═════════════════════════════════════════════════╗" << endl;
-                    cout << "║  Login successful! Welcome, " << name << "!     ║" << endl;
-                    cout << "╚═════════════════════════════════════════════════╝" << endl;
-                }
-            break;
+    string credential1, credential2;
+    cin >> credential1;
+    cin >> credential2;
 
-            case 2:
-                cout << "╔═════════════════════════════════════════════════╗" << endl;
-                cout << "║  You chose to login with SPID                   ║" << endl;
-                cout << "║  Please enter your SPID credentials:            ║" << endl;
-                cout << "║  E-mail:                                        ║" << endl;
-                cout << "║  Password:                                      ║" << endl;
-                cout << "╚═════════════════════════════════════════════════╝" << endl;
-                cin >> email;
-                cin >> password;
-
-                if (email == "wrong" || password == "wrong") {
-                    cout << "╔═════════════════════════════════════════════════╗" << endl;
-                    cout << "║  Invalid credentials. Please try again.         ║" << endl;
-                    cout << "╚═════════════════════════════════════════════════╝" << endl;
-                    return false;
-                }
-                else{
-                    cout << "╔═════════════════════════════════════════════════╗" << endl;
-                    cout << "║  Login successful! Welcome, " << name << "!     ║" << endl;
-                    cout << "╚═════════════════════════════════════════════════╝" << endl;
-                }
-            break;
-
-            default:
-                cout << "╔═════════════════════════════════════════════════╗" << endl;
-                cout << "║  Your choice is invalid. Please enter 1 or 2.   ║" << endl;
-                cout << "╚═════════════════════════════════════════════════╝" << endl;
-                return false; 
-    return true;
+    if (credential1 != email || credential2 != password) {
+        cout << "╔═════════════════════════════════════════════════╗" << endl;
+        cout << "║  Invalid credentials. Please try again.         ║" << endl;
+        cout << "╚═════════════════════════════════════════════════╝" << endl;
+    }else{
+        cout << "╔═════════════════════════════════════════════════╗" << endl;
+        cout << "║  Login successful! Welcome, " << name << "!     ║" << endl;
+        cout << "╚═════════════════════════════════════════════════╝" << endl;
     }
 }
