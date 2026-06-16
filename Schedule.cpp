6ff24e147fd06c8fa526486e520776609acb2329
elquini33 <silviazora12@gmail.com>
2026-06-16 16:56:23 +0200

    Update Schedule.cpp

--- Schedule.cpp
+++ Schedule.cpp
@@ -1,16 +1,58 @@
 #include "Schedule.h"
+#include <iostream>
 
-Schedule::Schedule(string doctorId,
-                   string date,
-                   string timeSlot)
+using namespace std;
+
+Schedule::Schedule()
 {
-    this->doctorId = doctorId;
-    this->date = date;
-    this->timeSlot = timeSlot;
+    slotId = "";
+    doctorId = "";
+    date = "";
+    timeSlot = "";
     available = true;
 }
 
-bool Schedule::isAvailable()
+Schedule::Schedule(const string& slotId,
+                   const string& doctorId,
+                   const string& date,
+                   const string& timeSlot,
+                   bool available)
 {
-    return available;
+    this->slotId = slotId;
+    this->doctorId = doctorId;
+    this->date = date;
+    this->timeSlot = timeSlot;
+    this->available = available;
+}
+
+string Schedule::getSlotId() const { return slotId; }
+string Schedule::getDoctorId() const { return doctorId; }
+string Schedule::getDate() const { return date; }
+string Schedule::getTimeSlot() const { return timeSlot; }
+bool Schedule::isAvailable() const { return available; }
+
+
+void Schedule::setDate(const string& d)
+{
+    date = d;
+}
+
+void Schedule::setTimeSlot(const string& t)
+{
+    timeSlot = t;
+}
+
+void Schedule::setAvailable(bool a)
+{
+    available = a;
+}
+
+void Schedule::display() const
+{
+    cout << "Slot: " << slotId
+         << " | Doctor: " << doctorId
+         << " | Date: " << date
+         << " | Time: " << timeSlot
+         << " | Available: " << (available ? "YES" : "NO")
+         << endl;
 }
